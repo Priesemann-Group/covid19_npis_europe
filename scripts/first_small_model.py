be2e361a3be93cdb991f984301c64eeec7a16b28
Sebastian <sebastian@mohrenclan.de>
2020-07-10 11:48:11 +0200

    Added wip script

---
+++ scripts/first_small_model.py
@@ -0,0 +1,19 @@
+# Append folder with covid19_npis package
+import sys
+
+sys.path.append("../")
+
+import covid19_npis
+
+
+""" # Data retrieval / Convertions
+"""
+
+""" # Model initilization
+"""
+
+""" # Sample
+"""
+
+""" # Plotting
+"""

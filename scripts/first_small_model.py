286987be2c578d330bd06335847b6a0a640ed133
Sebastian <sebastian@mohrenclan.de>
2020-07-10 13:10:00 +0200

    Update to age group retrieval

--- scripts/first_small_model.py
+++ scripts/first_small_model.py
@@ -2,7 +2,7 @@
 import sys
 import datetime
 import pandas as pd
-
+import numpy as np
 
 sys.path.append("../")
 sys.path.append("../covid19_inference/")
@@ -17,7 +17,18 @@ import covid19_inference as cov19inf
 data_begin = datetime.datetime(2020, 3, 1)
 data_end = datetime.datetime(2020, 3, 20)
 countries = ["Belgium", "Czechia", "Latvia", "Portugal", "Switzerland"]
-
+age_groups = [
+    "0-9",
+    "10-19",
+    "20-29",
+    "30-39",
+    "40-49",
+    "50-59",
+    "60-69",
+    "70-79",
+    "80-89",
+    "90-99",
+]
 
 # Retriev data via covid19_inference toolbox
 # and create a dictionary for the different retrieval classes and download the datasets
@@ -25,10 +36,10 @@ retrieval = {}
 for country in countries:
     retrieval[country] = getattr(cov19inf.data_retrieval, country)(True)
 
-
+# Get all age groups and append to dataframe
 df = pd.DataFrame(index=pd.date_range(data_begin, data_end))
 for country in countries:
-    for age_group in ["0-9", "10-19", "20-29", "30-39"]:
+    for age_group in age_groups:
         df = df.join(
             retrieval[country].get_new(
                 data_begin=data_begin, data_end=data_end, age_group=age_group
@@ -36,7 +47,15 @@ for country in countries:
         )
 
 # Reindex to use multiindex
-df = df.reindex(columns=pd.MultiIndex.from_tuples(df.columns.values))
+df = df.reindex(
+    columns=pd.MultiIndex.from_tuples(df.columns.values, names=["country", "age_group"])
+)
+
+# Convert every number to int
+df = df.apply(lambda col: col.apply(lambda x: int(x) if pd.notnull(x) else x), axis=1)
+# Replace every 0 with np.nan
+df = df.replace(0, np.nan)
+
 print(df)
 
 """ ## 

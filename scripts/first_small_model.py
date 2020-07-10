b5b0a9d7baec1f1fe605c6402c2c9989b6c0d3b1
Sebastian <sebastian@mohrenclan.de>
2020-07-10 12:50:13 +0200

    Added data retrieval for age groups to example script we may want to make a function out of that

--- scripts/first_small_model.py
+++ scripts/first_small_model.py
@@ -1,13 +1,47 @@
 # Append folder with covid19_npis package
 import sys
+import datetime
+import pandas as pd
+
 
 sys.path.append("../")
-
+sys.path.append("../covid19_inference/")
 import covid19_npis
+import covid19_inference as cov19inf
 
 
 """ # Data retrieval / Convertions
 """
+
+# Configs
+data_begin = datetime.datetime(2020, 3, 1)
+data_end = datetime.datetime(2020, 3, 20)
+countries = ["Belgium", "Czechia", "Latvia", "Portugal", "Switzerland"]
+
+
+# Retriev data via covid19_inference toolbox
+# and create a dictionary for the different retrieval classes and download the datasets
+retrieval = {}
+for country in countries:
+    retrieval[country] = getattr(cov19inf.data_retrieval, country)(True)
+
+
+df = pd.DataFrame(index=pd.date_range(data_begin, data_end))
+for country in countries:
+    for age_group in ["0-9", "10-19", "20-29", "30-39"]:
+        df = df.join(
+            retrieval[country].get_new(
+                data_begin=data_begin, data_end=data_end, age_group=age_group
+            )
+        )
+
+# Reindex to use multiindex
+df = df.reindex(columns=pd.MultiIndex.from_tuples(df.columns.values))
+print(df)
+
+""" ## 
+"""
+
 
 """ # Model initilization
 """

Correct numerical instability of gamma kernel

covid19_npis/model/disease_spread.py
@@ -273,7 +273,7 @@ def construct_generation_interval(
     )
 
     log.debug(f"g_mu:\n{g_mu}")
-    log.debug(f"g_mu:\n{g_theta}")
+    log.debug(f"g_theta:\n{g_theta}")
 
     g_theta = tf.expand_dims(g_theta, axis=-1)
     g_mu = tf.expand_dims(g_mu, axis=-1)

covid19_npis/model/likelihood.py
@@ -24,17 +24,17 @@ def studentT_likelihood(modelParams, new_cases):
     )
     sigma = sigma[..., tf.newaxis, :, tf.newaxis]
 
-    log.debug(f"sigma:\n{sigma.shape}")
+    log.debug(f"sigma:\n{sigma}")
     # Likelihood of the data
     data = modelParams.data_tensor
     mask = ~np.isnan(data)
     len_batch_shape = len(new_cases.shape) - 3
 
-    log.debug(f"data:\n{tf.boolean_mask(data, mask).shape}")
-    log.debug(f"new_cases without mask:\n{new_cases.shape}")
+    log.debug(f"data:\n{tf.boolean_mask(data, mask)}")
+    log.debug(f"new_cases without mask:\n{new_cases}")
 
     log.debug(
-        f"new_cases w. mask:\n{tf.boolean_mask(new_cases, mask, axis=len_batch_shape).shape}"
+        f"new_cases w. mask:\n{tf.boolean_mask(new_cases, mask, axis=len_batch_shape)}"
     )
 
     likelihood = yield pm.StudentT(
@@ -47,5 +47,6 @@ def studentT_likelihood(modelParams, new_cases):
         observed=tf.boolean_mask(data, mask),
         reinterpreted_batch_ndims=1,
     )
-    log.debug(f"likelihood:\n{likelihood.shape}")
+    log.debug(f"likelihood:\n{likelihood}")
+    tf.debugging.check_numerics(likelihood, "Nan in likelelihood", name="likelihood")
     return likelihood

covid19_npis/model/reproduction_number.py
@@ -279,6 +279,7 @@ def construct_R_t(R_0, modelParams):
     """ We want to create a time dependent R_t for each country and age group
         We iterate over country and interventions.
     """
+
     # Create time index tensor of length modelParams.simlength
     t = tf.range(0, modelParams.length, dtype="float32")
 
@@ -299,7 +300,7 @@ def construct_R_t(R_0, modelParams):
                 gammas_cp.append(gamma_cp)
             log.debug(f"gammas_cp:\n{gammas_cp}")
             gamma_t = tf.reduce_sum(gammas_cp, axis=0)
-            log.debug(f"gamma_t reduced_sum:\n{gammas_cp}")
+            log.debug(f"gamma_t reduced_sum:\n{gamma_t}")
             # Append gamma*alpha to array
             _sum.append(tf.einsum("...ai,...->...ai", gamma_t, alpha_interv))
 

covid19_npis/model/utils.py
@@ -8,9 +8,11 @@ import numpy as np
 log = logging.getLogger(__name__)
 
 
-# Un-normalized distribution pdf for generation interval
+# Distribution pdf for generation interval
 def gamma(x, alpha, beta):
-    return tf.math.pow(x, (alpha - 1)) * tf.exp(-beta * x)
+    log_unnormalized_prob = tf.math.xlogy(alpha - 1.0, x) - beta * x
+    log_normalization = tf.math.lgamma(alpha) - alpha * tf.math.log(beta)
+    return tf.exp(log_unnormalized_prob - log_normalization)
 
 
 # Un-normalized distribution pdf for generation interval
3059c50d085643caf18f55901907ad96dc733df0
jdehning <jonas.dehning@ds.mpg.de>
2021-01-17 18:03:21 +0100

    Add diagonal mass matrix optimization

--- scripts/dummy_model.py
+++ scripts/dummy_model.py
@@ -26,9 +26,12 @@ os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
 """
 import pymc4 as pm
 import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 import time
 import os
+import matplotlib.pyplot as plt
+import arviz as az
 
 """
 SM: I dont know what this is doing, please explain :)
@@ -131,14 +134,24 @@ print_dist_shapes(sample_state)
 
 begin_time = time.time()
 log.info("start")
-trace = pm.sample(
+num_chains = 4
+
+
+trace_tuning, trace = pm.sample(
     this_model,
-    num_samples=50,
-    burn_in=50,
+    num_samples=60,
+    num_samples_binning=10,
+    burn_in_min=10,
+    burn_in=100,
     use_auto_batching=False,
-    num_chains=2,
-    xla=True,
-    step_size=0.01
+    num_chains=num_chains,
+    xla=False,
+    initial_step_size=0.00001,
+    ratio_tuning_epochs=1.3,
+    max_tree_depth=4,
+    decay_rate=0.75,
+    target_accept_prob=0.75,
+    # num_steps_between_results = 9,
     #    state=pm.evaluate_model_transformed(this_model)[1]
     # sampler_type="nuts",
 )
@@ -146,6 +159,77 @@ trace = pm.sample(
 end_time = time.time()
 log.info("running time: {:.1f}s".format(end_time - begin_time))
 
+"""
+step_size = np.mean(trace.sample_stats.isel(draw=-1)["step_size"])
+step_sizes = []
+print("Burn-in finished")
+for n_draws in n_window:
+    trace, step_size = adapting_sampling(trace, sampler_state, n_draws, step_size)
+    step_sizes.append(step_size)
+"""
+
+
+plt.plot(trace_tuning.sample_stats["step_size"][0])
+plt.figure()
+plt.plot(trace_tuning.sample_stats["lp"].T)
+plt.show()
+
+"""
+fpath = '/home/jdehning/repositories/covid19_npis_europe/scripts/traces/'
+name = '21_01_08_21'
+modelParams, trace = covid19_npis.utils.load_trace(name, fpath)
+values = {
+    var_name: tf.convert_to_tensor(value)
+    for var_name, value in trace.posterior.isel(draw=-1).items()
+}
+num_chains = 2
+trace, sampler_state = pm.sample(
+    this_model,
+    init=values,
+    num_samples=3,
+    burn_in=0,
+    use_auto_batching=False,
+    num_chains=None,
+    xla=False,
+    num_adaptation_steps=100,
+    step_size=0.00001,
+    max_tree_depth=3,
+    max_energy_diff=1e5,
+    reduce_fn=tfp.math.reduce_log_harmonic_mean_exp,
+    target_accept_prob=0.25,
+    return_sampler_state = True
+    #    state=pm.evaluate_model_transformed(this_model)[1]
+    # sampler_type="nuts",
+)
+values = {
+    var_name: tf.convert_to_tensor(value)
+    for var_name, value in trace.posterior.isel(draw=-1).items()
+}
+
+trace, sampler_state = pm.sample(
+    this_model,
+    init=values,
+    num_samples=3,
+    burn_in=0,
+    use_auto_batching=False,
+    num_chains=None,
+    xla=False,
+    num_adaptation_steps=100,
+    step_size=0.00001,
+    max_tree_depth=3,
+    max_energy_diff=1e5,
+    reduce_fn=tfp.math.reduce_log_harmonic_mean_exp,
+    target_accept_prob=0.25,
+    return_sampler_state = True,
+    sampler_state = sampler_state
+    #    state=pm.evaluate_model_transformed(this_model)[1]
+    # sampler_type="nuts",
+)
+
+_, state = pm.evaluate_model_posterior_predictive(
+    this_model, values=values, sample_shape=(2,)
+)
+"""
 
 # We also Sample the prior for the kde in the plots (optional)
 trace_prior = pm.sample_prior_predictive(
